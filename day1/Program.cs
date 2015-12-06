a3dfad9be89d40c7c9d8381a42ee0ea1f8ac8b62
CyberSaving <alex.jabba@gmail.com>
2015-12-06 14:38:47 +0100

    day1

---
+++ day1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filestr = File.ReadAllText(@"input.txt");
+            decimal r = filestr.Sum( ch => (ch == '(' ? 1 : -1));
+            Console.WriteLine("result day1.1 = {0}.", r);
+            
+            r = 0;
+            int i=0;
+            for (i = 0; i < filestr.Length; i++)
+			{
+                
+			    r += (filestr[i]=='(' ? 1 : -1 );
+                if (r == -1)
+                    break;
+			}
+            Console.WriteLine( "result day1.2 = {0}.\n", i+1);
+            Console.Write("Presse eny key ...");
+            Console.ReadKey();
+        }
+    }
+}
