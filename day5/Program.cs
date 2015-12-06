6c89555f702fc548c188dfb3b052ad91ede2d39c
CyberSaving <alex.jabba@gmail.com>
2015-12-07 00:50:20 +0100

    add days 5 and 6

---
+++ day5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] filelns = File.ReadAllLines(@"input.txt");
+            Regex reg1 = new Regex(@"(\w{2})\w*\1");
+            Regex reg2 = new Regex(@"(\w)\w\1");
+
+            int goodstring =0;
+            int goodstring2=0;
+            
+            foreach (string l in filelns)
+            {
+                int hmvowels = 0;
+                bool hasdouble = false;
+                bool hasfalsestr = false;
+                char cprev = '\0';
+                for (int i = 0; i < l.Length; i++)
+                {
+                    char ccur = l[i];
+                    if ("aeiou".IndexOf(ccur) >= 0)
+                        hmvowels++;
+
+                    if (cprev == ccur)
+                        hasdouble = true;
+                    if ((cprev == (char)(ccur - 1)) && "acpx".IndexOf(cprev) >= 0)
+                        hasfalsestr = true;
+
+                    cprev = ccur;
+                }
+                if (!hasfalsestr && hasdouble && hmvowels >= 3)
+                    goodstring++;
+                if (reg1.IsMatch(l) && reg2.IsMatch(l))
+                    goodstring2++;
+            }
+            Console.WriteLine("result day5.1 = {0}.\n", goodstring);
+            Console.WriteLine("result day5.2 = {0}.\n", goodstring2);
+
+            Console.Write("Presse eny key ...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ day6/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] filelns = File.ReadAllLines(@"input.txt");
+            Regex r = new Regex(@"(turn off|turn on|toggle) (\d+),(\d+) through (\d+),(\d+)");
+
+            byte[,] lights = new byte[1000, 1000];
+            short[,] lightsbr = new short[1000, 1000];
+
+
+
+            int count = 0;
+            long countbr = 0;
+            foreach (string l in filelns)
+            {
+                var ms =r.Match(l);
+                int fromx = int.Parse(ms.Groups[2].Value);
+                int fromy = int.Parse(ms.Groups[3].Value);
+
+                int tox = int.Parse(ms.Groups[4].Value);
+                int toy = int.Parse(ms.Groups[5].Value);
+
+                int action = (ms.Groups[1].Value=="turn off") ? 0 : (ms.Groups[1].Value=="toggle") ? -1 : 1;
+
+
+                for (int x = fromx; x <= tox; x++)
+                    for (int y = fromy; y <= toy; y++){
+                        if (action < 0){
+                            
+                            //for test 1
+                            if(lights[x, y]==1){
+                                count --;
+                                lights[x, y] =0;  
+                            }else{
+                                count ++;
+                                lights[x, y] =1;
+                            }
+                            //for test 2
+
+                            lightsbr[x, y] += 2;
+                            countbr += 2;
+                        }
+                        else
+                        {
+                            //for test 1
+                            if (lights[x, y] != action){
+                                count += (action * 2) - 1;
+                                lights[x, y] = (byte)action;
+                            }
+
+                            //for test 2
+                            if (action==0){
+                                if (lightsbr[x, y] > 0)
+                                {
+                                    lightsbr[x, y]--;
+                                    countbr--;
+                                }
+                            }else{
+                                lightsbr[x, y]++;
+                                countbr ++;
+                            }
+                        }
+                            
+                    }
+
+            }
+            Console.WriteLine("result day6.1 = {0}.\n", count);
+            Console.WriteLine("result day6.2 = {0}.\n", countbr);
+            
+            Console.Write("Presse eny key ...");
+            Console.ReadKey();
+        }
+    }
+}
